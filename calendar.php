4006b4974a6774588f8ed87f703eb9d744cf1b90
llevieux <llevieux@lucaslevieux.com>
2016-01-19 23:07:45 -0800

    removed links and html detritus

--- calendar.php
+++ calendar.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-		<link href='fullcalendar/fullcalendar.css' rel='stylesheet' />
-		<link href='fullcalendar/fullcalendar.print.css' rel='stylesheet' media='print' />
-		<script src='fullcalendar/lib/moment.min.js'></script>
-		<script src='fullcalendar/lib/jquery.min.js'></script>
-		<script src='fullcalendar/fullcalendar.min.js'></script>
-		<script src='fullcalendar/gcal.js'></script>
-		<script type='text/javascript'>
-
+<script type='text/javascript'>
 $(document).ready(function() {
 	$('#calendar').fullCalendar({
 		googleCalendarApiKey: 'AIzaSyAZTD6_f2aFg9LwRXATJPAXJ2PP_WeN-Fo',
@@ -34,54 +23,51 @@ $(document).ready(function() {
 	});
 });
 
-		</script>
-		<style>
+</script>
+<style>
+#loading {
+	display: none;
+	position: absolute;
+	top: 10px;
+	right: 10px;
+}
 
-			#loading {
-				display: none;
-				position: absolute;
-				top: 10px;
-				right: 10px;
-			}
+#calendar {
+	max-width: 900px;
+	margin: 0 auto;
+}
+#calendar table{
+	margin: 0;
+}
+#calendar td.fc-event-container a{
+	height: 80px;
+	background-color: black;
+	border: none;
+}
+div.fc-content{
+	overflow: visible;
+	white-space: normal;
+	padding: 5px;
+	text-align: center;
 
-			#calendar {
-				max-width: 900px;
-				margin: 0 auto;
-			}
-			#calendar td.fc-event-container a{
-				height: 80px;
-				background-color: black;
-				border: none;
-			}
-			div.fc-content{
-				overflow: visible;
-				white-space: normal;
-				padding: 5px;
-				text-align: center;
-				
-			}
-			div.fc-content span.fc-time{
-				overflow: visible;
-				white-space: normal;
-				display: block;
-				font-weight: normal;
-				font-size: 10pt;
-			}
-			
-			div.fc-content span.fc-title{
-				overflow: visible;
-				white-space: normal;
-				margin-top: 10px;
-				display: block;
-				font-weight: bold;
-				font-size: 12pt;
-			}
-				
-		</style>
-        <title></title>
-    </head>
-    <body>
-	<div id='loading'>loading...</div>
-	<div id='calendar'></div>
-    </body>
-</html>
+}
+div.fc-content span.fc-time{
+	overflow: visible;
+	white-space: normal;
+	display: block;
+	font-weight: normal;
+	font-size: 10pt;
+}
+
+div.fc-content span.fc-title{
+	overflow: visible;
+	white-space: normal;
+	margin-top: 10px;
+	display: block;
+	font-weight: bold;
+	font-size: 12pt;
+}		
+</style>
+
+<div id='loading'>loading...</div>
+<div id='calendar'></div>
