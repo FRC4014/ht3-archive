5be95c44b7f0fc83b1ce60f2ee4afd8017647dc4
llevieux <llevieux@lucaslevieux.com>
2016-01-20 22:16:21 -0800

    change description and version number

--- special.php
+++ special.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: HT^3 Special Pages
 Plugin URI:  https://github.com/FRC4014/ht3-archive
-Description: Replaces certain pages with a code!  Cool.
-Version:     2.0
+Description: Replaces certain pages with certain code.  Currently does a calendar and an archive page, more to come.
+Version:     2.1
 Author:      Lucas LeVieux
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
